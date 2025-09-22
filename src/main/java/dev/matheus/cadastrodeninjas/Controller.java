cf16cd3789f5097dd2cb4b52a4e5aa6e670582b0
Matheus Bonato Veloso Batista <bqnato@gmail.com>
2025-09-22 18:50:43 -0300

    criado o arquivo controller

---
+++ src/main/java/dev/matheus/cadastrodeninjas/Controller.java
@@ -0,0 +1,17 @@
+package dev.matheus.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Cadastro de Ninjas!";
+    }
+
+
+}
